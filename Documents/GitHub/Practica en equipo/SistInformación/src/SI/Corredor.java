71a18935afd131771b6ddd5649f89bbff9efaad4
pabloSI <pablo3vias@gmail.com>
2016-02-10 17:50:59 +0100

    Update Corredor.java

--- Documents/GitHub/Practica en equipo/SistInformación/src/SI/Corredor.java
+++ Documents/GitHub/Practica en equipo/SistInformación/src/SI/Corredor.java
@@ -2,6 +2,15 @@ package SI;
 
 public class Corredor {
   
-  
+  private String nombre;
+  private String apellido1;
+  private String apellido2;
+  private int edad;
+  public Corredor(String nombre,String ap1,String ap2,int edad){
+    this.nombre=nombre;
+    this.apellido1=ap1;
+    this.apellido2=ap2;
+    this.edad=edad;
+  }
 
 }
